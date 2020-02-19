f88b428722910c25d557c5ba30da3557fb5cc8c5
Karthika D <karthikacseksrct@gmail.com>
2020-02-19 23:21:37 +0530

    Add project files.

---
+++ OnlineRealEstate/Controllers/UserController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using OnlineRealEstateEntity;
+using System.Web.Mvc;
+
+namespace OnlineRealEstate.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet] 
+        public ActionResult SignUp()
+        {
+            ViewBag.Message = "Register successfull";
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Login()
+        {
+            ViewBag.Message = "Login Successfull";
+            return View();
+        }
+    }
+}
